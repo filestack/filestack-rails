315aed73b7bd18d25aa4ec9c72cb2dfb8094ddac
Tobias Maier <info@tobiasmaier.info>
2013-02-27 15:24:07 +0100

    Refactor filepicker_field
    * allow to define arbitrary options
    * update option naming to current API

--- lib/filepicker/rails/form_builder.rb
+++ lib/filepicker/rails/form_builder.rb
@@ -3,37 +3,32 @@ module Filepicker
     module FormBuilder
 
       def filepicker_field(method, options = {})
-        input_options = {
-          'data-fp-apikey' =>
-          ::Rails.application.config.filepicker_rails.api_key,
+        type = options.delete(:dragdrop) ? 'filepicker-dragdrop' : 'filepicker'
 
-          'data-fp-button-text' => options.fetch(:button_text, "Pick File"),
-
-          'data-fp-button-class' => options[:button_class],
-
-          'data-fp-mimetypes' => options[:mimetypes],
-
-          'data-fp-option-container' => options[:container],
-
-          'data-fp-option-multiple' => false,
-
-          'data-fp-option-services' => Array(options[:services]).join(","),
-
-          'data-fp-drag-text' => options.fetch(:drag_text, "Or drop files here"),
-
-          'data-fp-drag-class' => options[:drag_class],
-
-          'onchange' => options[:onchange],
-          
-          'class' => options[:class],
-          
-          'value' => options[:value]
-        }
-
-        type = options[:dragdrop] ? 'filepicker-dragdrop' : 'filepicker'
+        input_options = retrive_legacy_filepicker_options(options)
+        input_options['data-fp-apikey'] ||= ::Rails.application.config.filepicker_rails.api_key
 
         ActionView::Helpers::InstanceTag.new(@object_name, method, @template)
           .to_input_field_tag(type, input_options)
+      end
+
+      private
+
+      def retrive_legacy_filepicker_options(options)
+        mappings = {
+            :button_text  => 'data-fp-button-text',
+            :button_class => 'data-fp-button-class',
+            :mimetypes    => 'data-fp-mimetypes',
+            :container    => 'data-fp-container',
+            :services     => 'data-fp-services',
+            :drag_text    => 'data-fp-drag-text',
+            :drag_class   => 'data-fp-drag-class',
+            :onchange     => 'onchange',
+            :class        => 'class',
+            :value        => 'value'
+        }
+
+        Hash[options.map {|k, v| [mappings[k] || k, v] }]
       end
     end
   end
