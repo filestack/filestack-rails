2ea7725eed0a804f0e52772acd04b02e2feb054b
Tobias Maier <info@tobiasmaier.info>
2013-03-12 15:04:56 +0100

    Add secret key as config variable

--- lib/filepicker/rails/configuration.rb
+++ lib/filepicker/rails/configuration.rb
@@ -1,9 +1,13 @@
 module Filepicker
   module Rails
     class Configuration
-      attr_writer :api_key
+      attr_writer :api_key, :secret_key
       def api_key
         @api_key or raise "Set config.filepicker_rails.api_key"
+      end
+
+      def secret_key
+        @secret_key
       end
     end
   end
