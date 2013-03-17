e228728ef8d3fd9dd138b5caa5af6a3b957bcbf1
Tobias Maier <me@tobiasmaier.info>
2013-03-17 08:53:33 +0100

    Add writer for default_expiry

--- lib/filepicker/rails/configuration.rb
+++ lib/filepicker/rails/configuration.rb
@@ -1,7 +1,7 @@
 module Filepicker
   module Rails
     class Configuration
-      attr_writer :api_key, :secret_key
+      attr_writer :api_key, :secret_key, :default_expiry
 
       def api_key
         @api_key or raise "Set config.filepicker_rails.api_key"
