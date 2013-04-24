7dacdf011c0fe5bf337f314436633b50772bf8c4
Ricardo Cruz <ricardocruz.a89@gmail.com>
2013-04-23 19:45:53 -0500

    fixed view helper

--- lib/filepicker/rails/view_helpers.rb
+++ lib/filepicker/rails/view_helpers.rb
@@ -23,8 +23,8 @@ module Filepicker
 
       # Allows options to be passed to filepicker_image_url and then falls back to normal Rails options for image_tag
       # If specifying html width, height, pass it down to filepicker for optimization
-      def filepicker_image_tag(url, options={})
-        image_tag(filepicker_image_url(url, options))
+      def filepicker_image_tag(url, image_options={}, image_tag_options={})
+        image_tag(filepicker_image_url(url, image_options), image_tag_options)
       end
 
       # w - Resize the image to this width.
