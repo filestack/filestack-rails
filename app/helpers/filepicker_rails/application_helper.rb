f6f95568ab759ac1bc6faaffaf114e8157a780ac
Mauro George <maurogot@gmail.com>
2013-07-27 19:01:38 -0300

    add specs to filepicker_save_button

--- app/helpers/filepicker_rails/application_helper.rb
+++ app/helpers/filepicker_rails/application_helper.rb
@@ -4,5 +4,20 @@ module FilepickerRails
     def filepicker_js_include_tag
       javascript_include_tag "//api.filepicker.io/v1/filepicker.js"
     end
+
+    def filepicker_save_button(text, url, mimetype, options = {})
+      options[:data] ||= {}
+      container = options.delete(:container)
+      services = options.delete(:services)
+      save_as = options.delete(:save_as_name)
+
+      options[:data]['fp-url'] = url
+      options[:data]['fp-apikey'] = ::Rails.application.config.filepicker_rails.api_key
+      options[:data]['fp-mimetype'] = mimetype
+      options[:data]['fp-option-container'] = container if container
+      options[:data]['fp-option-services'] = Array(services).join(",") if services
+      options[:data]['fp-option-defaultSaveasName'] = save_as if save_as
+      button_tag(text, options)
+    end
   end
 end
--- spec/helpers/aplication_helper_spec.rb
+++ spec/helpers/aplication_helper_spec.rb
@@ -5,7 +5,53 @@ describe FilepickerRails::ApplicationHelper do
   describe "#filepicker_js_include_tag" do
 
     it "have correct output" do
-      filepicker_js_include_tag.should eq(%{<script src="//api.filepicker.io/v1/filepicker.js"></script>})
+      expect(filepicker_js_include_tag).to eq(%{<script src="//api.filepicker.io/v1/filepicker.js"></script>})
+    end
+  end
+
+  describe "#filepicker_save_button" do
+
+    context "without options" do
+
+      it "have the correct button" do
+        html = %{<button data-fp-apikey="123filepickerapikey"}
+        html << %{ data-fp-mimetype="image/jpg" data-fp-url="/foo" name="button"}
+        html << %{ type="submit">save</button>}
+        expect(filepicker_save_button('save', '/foo', 'image/jpg')).to eq(html)
+      end
+    end
+
+    context "with options" do
+
+      describe "container" do
+
+        it "have the correct button" do
+          html = %{<button data-fp-apikey="123filepickerapikey"}
+          html << %{ data-fp-mimetype="image/jpg" data-fp-option-container=\"modal\" data-fp-url="/foo"}
+          html << %{ name="button" type="submit">save</button>}
+          expect(filepicker_save_button('save', '/foo', 'image/jpg', container: 'modal')).to eq(html)
+        end
+      end
+
+      describe "services" do
+
+        it "have the correct button" do
+          html = %{<button data-fp-apikey="123filepickerapikey"}
+          html << %{ data-fp-mimetype="image/jpg" data-fp-option-services="COMPUTER, FACEBOOK"}
+          html << %{ data-fp-url="/foo" name="button" type="submit">save</button>}
+          expect(filepicker_save_button('save', '/foo', 'image/jpg', services: 'COMPUTER, FACEBOOK')).to eq(html)
+        end
+      end
+
+      describe "save_as_name" do
+
+        it "have the correct button" do
+          html = %{<button data-fp-apikey="123filepickerapikey"}
+          html << %{ data-fp-mimetype="image/jpg" data-fp-option-defaultSaveasName="myfile"}
+          html << %{ data-fp-url="/foo" name="button" type="submit">save</button>}
+          expect(filepicker_save_button('save', '/foo', 'image/jpg', save_as_name: 'myfile')).to eq(html)
+        end
+      end
     end
   end
 end
