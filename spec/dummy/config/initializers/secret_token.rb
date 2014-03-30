25044479d53ed764b228e461819a69f08c505df1
Mauro George <maurogot@gmail.com>
2014-03-30 10:25:13 -0300

    Fix specs on rails 3.2

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 Dummy::Application.config.secret_key_base = 'f76d42b4a5ac49ade652d7bb814336d6ca1658b55c2645a76288715cf1320935d8c662ecc3149a71748b7f3d51a26a1347f46b53ce55a71051e5ee0f9c07a88b'
+Dummy::Application.config.secret_token = 'c829779725e6a58feb756bbadea4b08dbd1403edd07cf21724782b57d1922e6cdf2ae640e5'
