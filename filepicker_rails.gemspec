907ae0856ca277bd8db55383965524afbe3669c0
Mauro George <maurogot@gmail.com>
2014-04-12 11:09:07 -0300

    Add pry

--- filepicker_rails.gemspec
+++ filepicker_rails.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'timecop'
   s.add_development_dependency 'appraisal'
   s.add_development_dependency 'capybara'
+  s.add_development_dependency 'pry'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
