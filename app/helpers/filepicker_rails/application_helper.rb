a23bdb2833dff20d9464f4918819ebcca27223e7
Mauro George <maurogot@gmail.com>
2014-08-04 19:27:13 -0300

    Add docs on application helper

--- app/helpers/filepicker_rails/application_helper.rb
+++ app/helpers/filepicker_rails/application_helper.rb
@@ -1,10 +1,33 @@
 module FilepickerRails
   module ApplicationHelper
 
+    # Creates a javascript tag to the filepicker JavaScript.
+    #
+    # #### Examples
+    #
+    #     filepicker_js_include_tag
+    #     # => <script src="//api.filepicker.io/v1/filepicker.js"></script>
     def filepicker_js_include_tag
       javascript_include_tag "//api.filepicker.io/v1/filepicker.js"
     end
 
+    # Creates a button allowing the user to download a file
+    # (or upload it to any of the supported services). Set the content of
+    # the button on the `text` parameter. The `url` of the content you want the user to save.
+    # Define the `mimetype` of the content. Accepts a optional `options` parameter.
+    #
+    # #### Options
+    #
+    # - `:container` - Where to show the file picker dialog can be `modal`,
+    # `window` or the id of an iframe on the page.
+    # - `:services` - What services your users can upload to. Ex: `BOX, COMPUTER, FACEBOOK`.
+    # - `:save_as_name` - A recommended file name. The user can override this.
+    #
+    # #### Examples
+    #
+    #     filepicker_save_button "Save", @user.filepicker_url, "image/jpg"
+    #     # => <button data-fp-apikey="..." data-fp-mimetype="image/jpg" data-fp-url="https://www.filepicker.io/api/file/hFHUCB3iTxyMzseuWOgG" name="button" type="submit">save</button>
+    #
     def filepicker_save_button(text, url, mimetype, options = {})
       options[:data] ||= {}
       container = options.delete(:container)
@@ -20,14 +43,21 @@ module FilepickerRails
       button_tag(text, options)
     end
 
-    # Allows options to be passed to filepicker_image_url and then falls back to normal Rails options
-    # for image_tag
-    # If specifying html width, height, pass it down to filepicker for optimization
+    # Creates a image tag of the `url`. Accepts the options to work on filepicker.io,
+    # see the valid options on `filepicker_image_tag` documentation. Accepts html options to the image tag,
+    # see the [image_tag](http://api.rubyonrails.org/classes/ActionView/Helpers/AssetTagHelper.html#method-i-image_tag)
+    # documentation for the valid options.
+    #
+    # #### Examples
+    #
+    #      filepicker_image_tag @user.filepicker_url, w: 160, h: 160, fit: 'clip'
+    #      # => <img src="https://www.filepicker.io/api/file/hFHUCB3iTxyMzseuWOgG/convert?w=160&h=160&fit=clip" />
+    #
     def filepicker_image_tag(url, image_options={}, image_tag_options={})
       image_tag(filepicker_image_url(url, image_options), image_tag_options)
     end
 
-    # Generates the full path of the image to the specified `url` accepts optional `options`
+    # Creates the full path of the image to the specified `url` accepts optional `options`
     # hash for configuration.
     #
     # #### Options
@@ -50,8 +80,6 @@ module FilepickerRails
     #     - `:exif` - will rotate the image automatically based on the exif data in the image.
     #     -  Other valid values are integers between 0 and 359, for degrees of rotation.
     #
-    # - `:cache` - Specifies if the image should be cached or not.
-    #
     # - `:crop` - Crops the image to a specified rectangle. The input to this parameter
     #        should be 4 numbers for `x,y,width,height` - for example,
     #        `10, 20, 200, 250` would select the 200x250 pixel rectangle starting
@@ -59,7 +87,7 @@ module FilepickerRails
     #        image.
     #
     # - `:format` - Specifies what format the image should be converted to, if any.
-    #          Possible values are "jpg" and "png". For "jpg" conversions, you
+    #          Possible values are `jpg` and `png`. For `jpg` conversions, you
     #          can additionally specify a quality parameter.
     #
     # - `:quality` - For jpeg conversion, specifies the quality of the resultant image.
@@ -71,11 +99,19 @@ module FilepickerRails
     #             image (not the original watermark).
     #
     # - `:waterposition` - Where to put the watermark relative to the base image.
-    #                 Possible values for vertical position are "top","middle",
-    #                 "bottom" and "left","center","right", for horizontal
+    #                 Possible values for vertical position are `top`,`middle`,
+    #                 `bottom` and `left`,`center`,`right`, for horizontal
     #                 position. The two can be combined by separating vertical
     #                 and horizontal with a comma. The default behavior
     #                 is bottom,right
+    #
+    # - `:cache` - Specifies if the image should be cached or not.
+    #
+    # #### Examples
+    #
+    #      filepicker_image_url @user.filepicker_url, w: 160, h: 160, fit: 'clip'
+    #      # => https://www.filepicker.io/api/file/hFHUCB3iTxyMzseuWOgG/convert?w=160&h=160&fit=clip
+    #
     def filepicker_image_url(url, options = {})
       FilepickerImageUrl.new(url, options).execute
     end
