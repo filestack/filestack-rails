248185d901f1cb3f863d00183962ad0b53a935cb
Richard Herbert <richard.alan.herbert@gmail.com>
2017-07-21 15:13:21 -0500

    add transform tests

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,7 +10,7 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.compile = true
-    config.filestack_rails.api_key = 'Af7SDtlubQPqhByVjt4qPz'
+    config.filestack_rails.api_key = 'API_KEY'
     config.filestack_rails.client_name = 'rich_client'
 
     # Settings in config/environments/* take precedence over those specified here.
