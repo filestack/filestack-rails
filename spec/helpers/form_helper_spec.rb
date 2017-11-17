ee86ba2f2212e0378c723ac270c8079316f002f8
Pierre <naudot.p@gmail.com>
2017-11-17 10:25:40 +0100

    Allow to customize input id

--- spec/helpers/form_helper_spec.rb
+++ spec/helpers/form_helper_spec.rb
@@ -5,10 +5,26 @@ RSpec.describe FilestackRails::ApplicationHelper do
   include FilestackRails::FormHelper
   include ActionView::Helpers
   describe '#filestack_field' do
+
     it "provides the correct form_tag" do
       output = filestack_field :picture, "some content"
       expect(output).to include("<button")
-      expect(output).to include("</button>")     
+      expect(output).to include("</button>")
+    end
+
+    it 'provides the default input id' do
+      @object = Object.new
+      output = filestack_field :picture, 'some content'
+      expect(output).to include('object_picture')
+    end
+
+    context 'when options input_id is set' do
+      let(:options) { {input_id: 'my_custom_id'} }
+
+      it 'provides the correct input_id' do
+        output = filestack_field :picture, 'some content', options
+        expect(output).to include('my_custom_id')
+      end
     end
   end
 end
